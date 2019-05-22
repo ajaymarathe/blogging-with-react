d263887a016cc2338fd2b02ad0fa25776437cc14
Ajay Marathe <ajay.marathe@xebecindia.com>
2019-05-22 15:09:36 +0530

    added quill text editor

--- src/components/editPost.js
+++ src/components/editPost.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import CKEditor from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ReactQuill from 'react-quill'; // ES6
+import 'react-quill/dist/quill.bubble.css'; // ES6
 
 class EditPost extends Component {
     constructor(props) {
@@ -20,14 +20,14 @@ class EditPost extends Component {
 
          this.onChangeTitle = this.onChangeTitle.bind(this);
          this.onChangeCategory = this.onChangeCategory.bind(this);
-         this.updateContent = this.updateContent.bind(this);
+         this.onBodyChange = this.onBodyChange.bind(this)
          this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     onChangeTitle(e){
         this.setState({
             editPost:{
-                title : e.target.value
+                title:  e.target.value
             }
         });
     }
@@ -38,21 +38,16 @@ class EditPost extends Component {
         });
     }
 
-    // onChange(e){
-    //     this.setState({
-    //         body: e.target.value
-    //     });
-    // }
-    updateContent(e){
-        this.setState({
-            body: e.target.value
-        });
+    onBodyChange(value) {
+        this.setState({ 
+            body:  value
+        })
     }
 
     handleSubmit(e){
         e.preventDefault();
 
-        let postData = [this.state.editPost.title, this.state.newCategory, this.state.body];
+        let postData = [this.state.editPost.title, this.state.newCategory, this.state.editPost.body];
         console.log(postData);
     }
 
@@ -65,11 +60,11 @@ class EditPost extends Component {
                         <div className="col-md-8">
                         <form onSubmit={this.handleSubmit}> 
                                 <div className="form-group">
-                                    <label >Title: </label>
+                                    <label >Title: {this.state.editPost.title} </label>
                                     <input type="text"  value={this.state.editPost.title || ''} onChange={this.onChangeTitle} className="form-control"  placeholder="Enter Title" />
                                 </div>
                                 <div className='form-group'>
-                                    <label>Category: </label>
+                                    <label>Category: {this.state.newCategory} </label>
                                     <select  value={this.state.newCategory || ''} onChange={this.onChangeCategory} className="custom-select" >
                                         { 
                                             Object.keys(categoryData).map(function (key) {
@@ -81,19 +76,8 @@ class EditPost extends Component {
                                     </select>
                                 </div>
                                 <div className="form-group">
-                                   <label> Body:</label>
-                                    <CKEditor
-                                        editor={ ClassicEditor }
-                                        value={this.state.editPost.body}
-                                        // onChange={this.updateContent}
-                                        onChange={ ( event, editor ) => {
-                                            const data = editor.getData();
-                                            this.setState({
-                                                body: data
-                                            });
-                                        }
-                                     }
-                                    />
+                                   <label> Body: {this.state.body}</label>
+                                   <ReactQuill theme="bubble" value={this.state.editPost.body || ''} onChange={this.onBodyChange} />
                                 </div>
                                 <div className="form-group">
                                     <button type="submit" className="btn btn-primary">Publish</button>
